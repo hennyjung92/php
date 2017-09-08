101532f50e9272e4579d67bb6d9d78658a7487bb
hyunji_jung <hennyjung92@gmail.com>
2017-09-08 17:13:06 +0900

    index.php 수정중

--- views/whopet/about.php
+++ views/whopet/about.php
@@ -1,7 +1,112 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hyunji_jung
- * Date: 2017. 9. 8.
- * Time: PM 4:38
- */
+<!DOCTYPE html PUBLIC>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>ABOUT</title>
+    <!-- Bootstrap core CSS -->
+    <link href="resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="resources/css/modern-business.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="resources/vendor/jquery/jquery.min.js"></script>
+    <script src="resources/vendor/popper/popper.min.js"></script>
+    <script src="resources/vendor/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<? include("views/template/header.php"); ?>
+<section class="py-5 bg-image-full" style="background-image: url('resources/img/boyanddog2.jpg');background-repeat: no-repeat;
+background-position: center left; background-attachment: fixed; opacity:0.8 !important;">
+    <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+    <div style="height: 250px;"></div>
+</section>
+<section class="py-5">
+    <div class="container">
+        <h1 class="text-center" style="font-weight: 400;">Why Choose Us</h1>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6 order-2">
+                <div class="p-5">
+                    <img class="img-fluid rounded-circle" style="width:400px;" src="resources/img/underground.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-6 order-1">
+                <div class="p-5">
+                    <h2 class="display-4">Building</h2>
+                    <p>지하/건물 내부에서 추적 가능</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="p-5">
+                    <img class="img-fluid" src="resources/img/tracking.png"  alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-5">
+                    <h2 class="display-4">Map</h2>
+                    <p>map에서 연결과 동시에 추적</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6 order-2">
+                <div class="p-5">
+                    <img class="img-fluid" src="resources/img/usingpc.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-6 order-1">
+                <div class="p-5">
+                    <h2 class="display-4">PC Available</h2>
+                    <p>스마트폰 없어도 PC로 추적 가능</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<br><br>
+<section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="p-5">
+                    <img class="img-fluid" src="resources/img/service.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-5">
+                    <h2 class="display-4">Unlimited Service</h2>
+                    <p>디바이스 구매시 무한 서비스 제공</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<br><br>
+<? include("views/template/footer.php"); ?>
+</body>
+</html>
