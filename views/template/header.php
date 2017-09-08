5df84e7670d953fa54462ff373bbe9b09bda787b
hyunji_jung <hennyjung92@gmail.com>
2017-09-08 17:05:03 +0900

    index.php 수정중

--- views/template/header.php
+++ views/template/header.php
@@ -1,23 +1,20 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-pageEncoding="UTF-8"%>
-<%@ page import="java.io.PrintWriter"%>
-<script type="text/javascript">
-    function management(){
-        alert("통합관리시스템 페이지로 이동합니다.");
-        alert("test: 홈으로 이동");
-        location.href="${pageContext.request.contextPath}/index.jsp";
-    }
-</script>
-<%
-String userID = null; // 아이디 담김
-if (session.getAttribute("userID") != null) {
-userID = (String) session.getAttribute("userID"); // 세션 값 가져오기
-}
-%>
+<!--<script type="text/javascript">-->
+<!--    function management(){-->
+<!--        alert("통합관리시스템 페이지로 이동합니다.");-->
+<!--        alert("test: 홈으로 이동");-->
+<!--        location.href="${pageContext.request.contextPath}/index.php";-->
+<!--    }-->
+<!--</script>-->
+<!--<%-->
+<!--String userID = null; // 아이디 담김-->
+<!--if (session.getAttribute("userID") != null) {-->
+<!--userID = (String) session.getAttribute("userID"); // 세션 값 가져오기-->
+<!--}-->
+<!--%>-->
 <!-- Navigation -->
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-faded fixed-top" style="background-color:white;" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="${pageContext.request.contextPath }/index.jsp"><img src="${pageContext.request.contextPath}/resources/img/whopet_logo.png" class="img-responsive" height="50"></a>
+        <a class="navbar-brand" href="index.php"><img src="resources/img/whopet_logo.png" class="img-responsive" height="50"></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -25,7 +22,7 @@ userID = (String) session.getAttribute("userID"); // 세션 값 가져오기
             <ul class="navbar-nav ml-auto">
                 <%if(userID == null){ %>
                 <li class="nav-item">
-                    <a class="nav-link" href="${pageContext.request.contextPath }/views/member/join.jsp">Join</a>
+                    <a class="nav-link" href="views/member/join.php">Join</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#myModal" data-toggle="modal" >Login</a>
@@ -37,25 +34,25 @@ userID = (String) session.getAttribute("userID"); // 세션 값 가져오기
           </span>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="${pageContext.request.contextPath}/logoutAction.jsp">Logout</a>
+                    <a class="nav-link" href="logoutAction.php">Logout</a>
                 </li>
 
                 <%} %>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="${pageContext.request.contextPath }/views/whopet/about.jsp">About</a>
+                    <a class="nav-link" href="views/whopet/about.php">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="${pageContext.request.contextPath }/views/whopet/service.jsp">Services</a>
+                    <a class="nav-link" href="views/whopet/service.php">Services</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Community
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-                        <a class="dropdown-item" href="${pageContext.request.contextPath }/views/whopet/notice.jsp">Notice</a>
-                        <a class="dropdown-item" href="${pageContext.request.contextPath }/views/review/review_list.jsp">Review</a>
-                        <a class="dropdown-item" href="${pageContext.request.contextPath }/views/whopet/gallery.jsp">Gallery</a>
+                        <a class="dropdown-item" href="views/whopet/notice.php">Notice</a>
+                        <a class="dropdown-item" href="views/review/review_list.php">Review</a>
+                        <a class="dropdown-item" href="views/whopet/gallery.php">Gallery</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
@@ -63,8 +60,8 @@ userID = (String) session.getAttribute("userID"); // 세션 값 가져오기
                         Help
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                        <a class="dropdown-item" href="${pageContext.request.contextPath }/views/whopet/qna.jsp">Q&A</a>
-                        <a class="dropdown-item" href="${pageContext.request.contextPath }/views/whopet/contact.jsp">Contact</a>
+                        <a class="dropdown-item" href="views/whopet/qna.php">Q&A</a>
+                        <a class="dropdown-item" href="views/whopet/contact.php">Contact</a>
                     </div>
                 </li>
                 <li class="nav-item">
@@ -85,11 +82,9 @@ userID = (String) session.getAttribute("userID"); // 세션 값 가져오기
                 <h5 class="modal-title">Welcome to WHOPET!</h5>
             </div>
             <div class="modal-body">
-                <form class="form-signin" method="post" action="${pageContext.request.contextPath}/loginAction.jsp" style="padding-top:20px;">
+                <form class="form-signin" method="post" action="loginAction.php" style="padding-top:20px;">
                     <input type="text" class="form-control" placeholder="id" name="userID" maxlength="20" autofocus=""><br>
                     <input type="password" class="form-control" placeholder="password" name="userPassword" maxlength="20"><br><br>
-                    <%-- <a href="${pageContext.request.contextPath}/member/find_id.do" class="forgot-password pull-right">forgot ID?</a><br>
-                    <a href="${pageContext.request.contextPath}/member/find_password.do" class="forgot-password pull-right" >forgot Password?</a><br><br> --%>
                     <span style="text-align:center;"><input type="submit" class="btn btn-primary center-block" value="Login"></span>
                 </form>
             </div>
