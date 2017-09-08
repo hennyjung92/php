a89d69e38591c511f72f9156b9e8e75eabe6d17d
hyunji_jung <hennyjung92@gmail.com>
2017-09-08 17:40:12 +0900

    index.php 수정중

--- views/whopet/service.php
+++ views/whopet/service.php
@@ -1,7 +1,153 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hyunji_jung
- * Date: 2017. 9. 8.
- * Time: PM 4:38
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>SERVICE</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../../resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="../../resources/css/modern-business.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="../../resources/vendor/jquery/jquery.min.js"></script>
+    <script src="../../resources/vendor/popper/popper.min.js"></script>
+    <script src="../../resources/vendor/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<? include("../template/header.php"); ?>
+<section class="py-5 bg-image-full" style="background-image: url('../../resources/img/boyanddog3.jpg');background-repeat: no-repeat;
+background-position: center center; background-attachment: fixed; opacity:0.8 !important;">
+    <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+    <div style="height: 250px;"></div>
+</section>
+
+<section id="services" class="bg-light" style="padding-bottom:70px;">
+    <section class="py-5">
+        <div class="container">
+            <h1 class="text-center" style="font-weight: 400; letter-spacing: 1px;">Services</h1>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4">
+                <br>
+                <i class="fa fa-map-o fa-2x" id="service_map" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4 my-4">
+                <br><br>
+                <h3>
+                    <!--   <small>Title</small> -->
+                </h3>
+                <p>실시간 실내, 실외, 범위 제한 없이 위치 추적</p>
+            </div>
+
+            <div class="col-lg-3 col-sm-6 text-center mb-4">
+                <br>
+                <i class="fa fa-bar-chart fa-2x" id="service_chart" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4 my-4">
+                <br><br>
+                <h3>
+                    <!-- <small>Title</small> -->
+                </h3>
+                <p>실시간 분실 방지 및 통계</p>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4">
+                <i class="fa fa-sign-out fa-2x" id="service_signout" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4 my-4">
+                <h3>
+                    <!--  <small>Title</small> -->
+                    <br>
+                </h3>
+                <p>실시간 나의 위치 파악 및 재난 대피 경로 파악</p>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4">
+                <i class="fa fa-cog fa-2x" id="service_cog" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-3 col-sm-6 text-center mb-4 my-4">
+                <br>
+                <h3>
+                    <!--  <small>Title</small> -->
+                </h3>
+                <p>Mobile App, PC Web을 통한 내 애완동물 위치 파악 및 상태 관리 기능</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="serviceSystem" style="padding-top:20px; padding-bottom:60px;">
+    <!-- Marketing Icons Section -->
+    <section class="py-5">
+        <div class="container" style="padding-bottom:20px;">
+            <h1 class="text-center" style="font-weight: 400; letter-spacing: 1px;">Service System</h1>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="span3" style="float: none; margin: 0 auto;">
+                <h2 class="text-center">
+                    <img src="../../resources/img/servicesystem.png" class="img-responsive">
+                </h2>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+</section>
+
+<section id="forWhom" class="bg-light" style="padding-top:10px; padding-bottom:90px;">
+    <section class="py-5">
+        <div class="container" style="padding-bottom:20px;">
+            <h1 class="text-center" style="font-weight: 400; letter-spacing: 1px;">For Whom?</h1>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="h4 col-lg-4 col-sm-4 text-center mb-4">
+                <i class="fa fa-shopping-basket fa-4x" aria-hidden="true" style="padding-bottom:30px; padding-top:15px;"></i>
+                <h5 style="font-size:15px;">Pet Shop</h5>
+            </div>
+            <div class="h4 col-lg-4 col-sm-4 text-center mb-4">
+                <i class="fa fa-hospital-o fa-5x" aria-hidden="true" style="padding-bottom:30px;"></i>
+                <h5 style="font-size:15px;">Hospital</h5>
+            </div>
+            <div class="h4 col-lg-4 col-sm-4 text-center mb-4">
+                <i class="fa fa-users fa-5x" aria-hidden="true" style="padding-bottom:30px;"></i>
+                <h5 style="font-size:15px;">Animal Owner</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="device" style=" padding-bottom:30px;">
+    <section class="py-5">
+        <div class="container">
+            <h1 class="text-center" style="font-weight: 400; letter-spacing: 1px;">Device</h1>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 portfolio-item">
+                <div class="h-200">
+                    <a href="#"><img src="../../resources/img/whopet_ring.png" class="img-responsive" style="padding-left: 100px; padding-bottom:36px; width:80%"></a>
+                </div>
+            </div>
+            <div class="col-lg-6 portfolio-item">
+                <div class="h-200">
+                    <a href="#"><img src="../../resources/img/dogneckbelt.png" class="img-responsive" style="padding-left: 100px; width:70%"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+</section>
+<? include("../template/footer.php"); ?></body>
+</html>
