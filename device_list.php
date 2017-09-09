69d133214f233308ad945a39cee1137055b5b031
hyunji_jung <hennyjung92@gmail.com>
2017-09-09 15:38:20 +0900

    index.php 수정중

--- device_list.php
+++ device_list.php
@@ -22,7 +22,7 @@
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
-    <script src=<"vendor/popper/popper.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
