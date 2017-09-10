e97a4b36bc117bd80208e97f810f45403ecc9228
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 03:05:47 +0900

    file upload test

--- deleteAction.php
+++ deleteAction.php
@@ -8,6 +8,16 @@ $wp_hp_member = member();
 if(!$wp_hp_member[wp_hp_id])Error("로그인 후 이용해주세요.");
 
 $wp_hp_review_no = $_GET[wp_hp_review_no];
+
+$query = "select * from wp_hp_reviewBBS wp_hp_review_no='$wp_hp_review_no' and wp_hp_member_id=$wp_hp_member[wp_hp_id]";
+$result = mysql_query($query,$connect);
+$data = mysql_fetch_array($result);
+
+if($data[file01]){
+    $del_file = './data/'.$data[file01];
+    if($data[file01] && is_file($del_file)) unlink($del_file); // unlink(실제 삭제할 변수명)
+}
+
 $query = "delete from wp_hp_reviewBBS where wp_hp_review_no ='$wp_hp_review_no' and wp_hp_member_id ='$wp_hp_member[wp_hp_id]'";
 mysql_query($query,$connect);
 ?>
