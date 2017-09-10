498bd8b6a5d02a5d3390b057b69e2bb0469d8962
hyunji_jung <hennyjung92@gmail.com>
2017-09-10 18:40:54 +0900

    비번 암호화 테스트

--- loginAction.php
+++ loginAction.php
@@ -15,20 +15,21 @@ mysql_query("set names utf8",$connect);
 $result = mysql_query($query,$connect);
 $wp_hp_member = mysql_fetch_array($result);
 
-if(!$wp_hp_id)Error("아이디를 입력하세요");
-else if(!$wp_hp_member[wp_hp_id])Error("존재하지 않는 회원 아이디입니다.");
+if(!$wp_hp_id){
+    Error("아이디를 입력하세요");}
+elseif(!$wp_hp_member[wp_hp_id])Error("존재하지 않는 회원 아이디입니다.");
 
-if(!$wp_hp_password)Error("비밀번호를 입력하세요");
-else if(!$wp_hp_member[wp_hp_password]!=$wp_hp_password)Error("비밀번호가 같지 않습니다.");
-
-if($wp_hp_member[wp_hp_id] and $wp_hp_member[$wp_hp_password]==$wp_hp_password) {
-    $tmp = $wp_hp_member[wp_hp_id] . "//" . $wp_hp_member[$wp_hp_password];
-    setcookie("", "", time() * 60 * 60 * 24, "/"); // 24시간동안 유효
+if(!$wp_hp_password){
+    Error("비밀번호를 입력하세요");
 }
+elseif(!$wp_hp_member[wp_hp_password]!=$wp_hp_password)Error("비밀번호가 같지 않습니다.");
 
+if($wp_hp_member[wp_hp_id] and $wp_hp_member[wp_hp_password]==$wp_hp_password) {
+    $tmp = $wp_hp_member[wp_hp_id] . "//" . $wp_hp_member[wp_hp_password];
+    setcookie("COOKIES", $tmp, time() * 60 * 60 * 24, "/"); // 24시간동안 유효
+}
 ?>
 <script>
 window.alert("로그인 되었습니다");
 location.href="index.php";
-
 </script>
