07b0e5ed7a7606332840c8310216cdaaae7351fa
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 00:26:03 +0900

    페이징 적용 테스트

--- review_detail.php
+++ review_detail.php
@@ -57,8 +57,6 @@ $data = mysql_fetch_array($result);
                 <a href="review_list.php" class="btn btn-outline-success">list</a>
                 <a href="review_update.php" class="btn btn-outline-primary">modify</a>
                 <a href="deleteAction.php" class="btn btn-outline-danger" onclick="return confirm('정말로 삭제하시겠습니까?')">delete</a>
-                <% 	}
-                %>
             </div>
             <div class="col-lg-2 mb-2"></div>
         </div>
