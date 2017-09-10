2d5f9b3b10ae5150d356bf1e0b6307c42ae28d40
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 00:41:02 +0900

    페이징 적용 테스트

--- review_detail.php
+++ review_detail.php
@@ -17,7 +17,12 @@ $wp_hp_review_no = $_GET[wp_hp_review_no];
         <h1 class="text-center display-4">Detail View</h1>
     </div>
 </section>
-
+<?
+$query = "select * from wp_hp_reviewBBS where wp_hp_review_no='$wp_hp_review_no'";
+mysql_query("set names utf8");
+$result = mysql_query($query,$connect);
+$data = mysql_fetch_array($result);
+?>
 <!-- Page Content -->
 <div class="container">
     <!-- Page Heading/Breadcrumbs -->
@@ -28,12 +33,6 @@ $wp_hp_review_no = $_GET[wp_hp_review_no];
             <div class="row">
                 <div class="col-lg-2"></div>
                 <div class="col-lg-8">
-                    <?
-                    $query = "select * from wp_hp_reviewBBS where wp_hp_review_no='$wp_hp_review_no'";
-                    mysql_query("set names utf8");
-                    $result = mysql_query($query,$connect);
-                    $data = mysql_fetch_array($result);
-                    ?>
                     <div class="form-group">
                         <label>TITLE</label>
                         <input type="text" class="form-control" name="wp_shopbbsTitle" maxlength="50" value="<?=$data[wp_hp_review_title]?>" readonly >
@@ -56,7 +55,7 @@ $wp_hp_review_no = $_GET[wp_hp_review_no];
             </div>
             <div class="text-center" style="padding-top:20px; padding-bottom:30px;">
                 <a href="review_list.php" class="btn btn-outline-success">list</a>
-                <a href="review_update.php?wp_hp_review_no=<?=$data[$wp_hp_review_no?>" class="btn btn-outline-primary">modify</a>
+                <a href="review_update.php?wp_hp_review_no=<?=$data[wp_hp_review_no]?>" class="btn btn-outline-primary">modify</a>
                 <a href="deleteAction.php" class="btn btn-outline-danger" onclick="return confirm('정말로 삭제하시겠습니까?')">delete</a>
             </div>
             <div class="col-lg-2 mb-2"></div>
