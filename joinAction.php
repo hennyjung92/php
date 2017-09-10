c16bb34e4724f7591e417f0abef04b0cf4010101
hyunji_jung <hennyjung92@gmail.com>
2017-09-10 14:29:57 +0900

    db test

--- joinAction.php
+++ joinAction.php
@@ -1,5 +1,5 @@
 <?header("content-type:text/html; charset=UTF-8");
-$connect = mysql_connect("localhost","whopet","whopet123!");
+$connect = mysql_connect("localhost","whopet","whopet123");
 mysql_select_db("whopet",$connect);
 if(!$connect){
     echo "연결에 실패하였습니다.".mysql_error();
