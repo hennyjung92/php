ec50b726522d59e4f454953fc43651b120536fc1
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 14:00:31 +0900

    file delete test

--- review_list.php
+++ review_list.php
@@ -21,7 +21,8 @@ background-position: bottom center; background-attachment: fixed; opacity:0.8 !i
         </div>
     </section>
 <?
-$wp_hp_field = $_GET[wp_hp_field];
+echo $wp_hp_field;
+// $wp_hp_field = $_GET[wp_hp_field];
 $_page = $_GET[_page];
 $view_total = 10; // 한 페이지에 보이는 수
 if(!$_page)($_page=1); // 페이지 번호가 지정이 안되었을 경우
