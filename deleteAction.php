a2e5e1b89800ad8de4453bb4d9f36bc9ad81a9b7
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 14:43:33 +0900

    file delete test

--- deleteAction.php
+++ deleteAction.php
@@ -8,7 +8,7 @@ $wp_hp_member = member();
 if(!$wp_hp_member[wp_hp_id])Error("로그인 후 이용해주세요.");
 
 $wp_hp_review_no = $_GET[wp_hp_review_no];
-
+$wp_hp_field = $_GET[wp_hp_field];
 $query = "select * from wp_hp_reviewBBS where wp_hp_review_no='$wp_hp_review_no' and wp_hp_member_id='$wp_hp_member[wp_hp_id]'";
 $result = mysql_query($query,$connect);
 $data = mysql_fetch_array($result);
@@ -24,5 +24,5 @@ mysql_query($query,$connect);
 
 <script>
     window.alert("글이 삭제되었습니다.");
-    location.href="review_list.php";
+    location.href="review_list.php?wp_hp_field=<?=$wp_hp_field?>";
 </script>
