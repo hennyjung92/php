d148b20db4ba6e9bd934bf6c0bc4998f6159c9e0
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 15:49:42 +0900

    file delete test

--- joinAction.php
+++ joinAction.php
@@ -13,7 +13,7 @@ if(!$wp_hp_id)Error("아이디를 입력하세요.");
 if($wp_hp_id=substr($wp_hp_id,"12"))Error("아이디를 12자까지만 허용됩니다.");
 if(preg_match("/[^a_z A-Z 0-9]/",$wp_hp_id))Error("아이디는 영문 소/대문자와 숫자만 허용됩니다.");
 
-if(!$wp_hp_password)Error("비밀번호를 입력하세요.");
+if(!$_POST[wp_hp_password])Error("비밀번호를 입력하세요.");
 if(!$wp_hp_name)Error("이름을 입력하세요.");
 if(!$wp_hp_tel)Error("휴대폰 번호를 입력하세요.");
 if(!$wp_hp_email)Error("이메일을 입력하세요.");
