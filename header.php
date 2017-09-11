222e048c6d21d471791b0c77219f3b4001d5b310
hyunji_jung <hennyjung92@gmail.com>
2017-09-11 10:37:26 +0900

    file delete test

--- header.php
+++ header.php
@@ -57,8 +57,9 @@ $wp_hp_member = member();
 
                 <? if($wp_hp_member[wp_hp_id]){?>
                     <li class="nav-item">
-                    <a class="nav-link" href="about.php"><i class="fa fa-user-circle fa-lg" aria-hidden="true"></i></a>
+                    <a class="nav-link" href="about.php"><i class="fa fa-user-circle fa-lg" aria-hidden="true"></i>
                     <? echo $wp_hp_member[wp_hp_name]."(".$wp_hp_member[wp_hp_id].")"; ?>
+                    </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="about.php"><i class="fa fa-sign-out fa-lg" aria-hidden="true"></i></a>
