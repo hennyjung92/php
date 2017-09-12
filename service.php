f12478d9021aa8e6eb09b1aa51a31edff13300fa
hyunji_jung <hennyjung92@gmail.com>
2017-09-12 17:42:54 +0900

    file delete test

--- service.php
+++ service.php
@@ -54,7 +54,7 @@ background-position: center center; background-attachment: fixed; opacity:0.8 !i
 <!--                      <small>Title</small>-->
                     <br>
                 </h3>
-                <p>실시간 나의 위치 파악 <br>및 재난 대피 경로 파악</p>
+                <p>실시간 나의 위치 및<br>재난 대피 경로 파악</p>
             </div>
             <div class="col-lg-3 col-sm-6 text-center mb-4">
                 <i class="fa fa-cog fa-2x" id="service_cog" aria-hidden="true"></i>
@@ -64,7 +64,7 @@ background-position: center center; background-attachment: fixed; opacity:0.8 !i
                 <h3>
 <!--                      <small>Title</small>-->
                 </h3>
-                <p>Mobile App, PC Web을 통한 <br>내 애완동물 위치 파악 및 상태 관리 기능</p>
+                <p>Mobile App, PC Web을 통한<br>내 애완동물 위치 파악 및 상태 관리 기능</p>
             </div>
         </div>
     </div>
