9396e88a4958123ba40157bc75456416d0dce005
hyunji_jung <hennyjung92@gmail.com>
2017-09-12 15:43:57 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -16,12 +16,10 @@
                 </a>
             </div>
             <div class="col-lg-3 col-sm-3">
-                <a href=“#”><img src="img/flag_ko.png" class="img-responsive"></a>
-                <a href=“#”><img src="img/flag_ko.png" class="img-responsive"></a>
-<!--                    <ul style="list-style: none; padding-right: 30px; padding-top: 10px;">-->
-<!--                        <li style="display: block;"><a href=“#”><img src="img/flag_ko.png" class="img-responsive"></a></li>-->
-<!--                        <li style="display: block;"><a href=“#”><img src="img/flag_en.png" class="img-responsive"></a></li>-->
-<!--                    </ul>-->
+                    <ul style="list-style: none; padding-right: 30px; padding-top: 10px;">
+                        <li style="display: block;"><a href=“#”><img src="img/flag_ko.png" class="img-responsive"></a>Korean</li>
+                        <li style="display: block;"><a href=“#”><img src="img/flag_en.png" class="img-responsive"></a>English</li>
+                    </ul>
             </div>
             <div class="col-lg-2 col-sm-2">
             </div>
