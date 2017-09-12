e85c7331539f1d3967c3cd27f0bc263f48dabac7
hyunji_jung <hennyjung92@gmail.com>
2017-09-12 15:37:59 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="py-5 bg-dark">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-sm-4" style="padding-top:20px;">
+            <div class="col-lg-3 col-sm-3" style="padding-top:20px;">
                 <a class="btn" href="https://www.facebook.com/" target="_blank" style="color:white">
                     <i class="fa fa-facebook" aria-hidden="true"></i>
                 </a>
@@ -15,13 +15,15 @@
                     <i class="fa fa-youtube-play" aria-hidden="true"></i>
                 </a>
             </div>
-            <div class="col-lg-4 col-sm-4">
+            <div class="col-lg-3 col-sm-3">
                     <ul style="list-style: none; padding-right: 30px; padding-top: 10px;">
                         <li style="display: block;"><a href=“#”><img src="img/flag_ko.png" class="img-responsive"></a></li>
                         <li style="display: block;"><a href=“#”><img src="img/flag_en.png" class="img-responsive"></a></li>
                     </ul>
             </div>
-            <div class="col-lg-4 col-sm-4">
+            <div class="col-lg-3 col-sm-3">
+            </div>
+            <div class="col-lg-3 col-sm-3">
                 <p class="m-0 text-right text-white">82-70-7517-6246</p>
                 <p class="m-0 text-right text-white">help@whopet.net</p>
                 <p class="m-0 text-right text-white">Hyojung BD 4Floor 15-1,Seoul,Korea</p>
