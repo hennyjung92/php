52b4c076336c3293f075478afbbb341b8e5314ac
hyunji_jung <hennyjung92@gmail.com>
2017-09-13 14:50:07 +0900

    file delete test

--- notice_list.php
+++ notice_list.php
@@ -50,87 +50,87 @@ $wp_hp_field = $_GET[wp_hp_field];
         ?>
 
         <!-- Project Two -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog2.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3 style="font-weight: 400;">Notice 2</h3>
-                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
-                <a class="btn btn-primary" href="#">View More
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
-        </div>
+<!--        <div class="row">-->
+<!--            <div class="col-md-7">-->
+<!--                <a href="#">-->
+<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog2.jpg" alt="">-->
+<!--                </a>-->
+<!--            </div>-->
+<!--            <div class="col-md-5">-->
+<!--                <h3 style="font-weight: 400;">Notice 2</h3>-->
+<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
+<!--                <a class="btn btn-primary" href="#">View More-->
+<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        </div>-->
         <!-- /.row -->
 
-        <hr>
+<!--        <hr>-->
 
         <!-- Project Three -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog3.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3 style="font-weight: 400;">Event 1</h3>
-                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
-                <a class="btn btn-primary" href="#">View More
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
-        </div>
+<!--        <div class="row">-->
+<!--            <div class="col-md-7">-->
+<!--                <a href="#">-->
+<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog3.jpg" alt="">-->
+<!--                </a>-->
+<!--            </div>-->
+<!--            <div class="col-md-5">-->
+<!--                <h3 style="font-weight: 400;">Event 1</h3>-->
+<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
+<!--                <a class="btn btn-primary" href="#">View More-->
+<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        </div>-->
         <!-- /.row -->
 
-        <hr>
+<!--        <hr>-->
 
         <!-- Project Four -->
-        <div class="row">
-
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog5.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3 style="font-weight: 400;">Event 2</h3>
-                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
-                <a class="btn btn-primary" href="#">View More
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
-        </div>
+<!--        <div class="row">-->
+<!---->
+<!--            <div class="col-md-7">-->
+<!--                <a href="#">-->
+<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog5.jpg" alt="">-->
+<!--                </a>-->
+<!--            </div>-->
+<!--            <div class="col-md-5">-->
+<!--                <h3 style="font-weight: 400;">Event 2</h3>-->
+<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
+<!--                <a class="btn btn-primary" href="#">View More-->
+<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        </div>-->
         <!-- /.row -->
-        <hr>
+<!--        <hr>-->
         <a href="notice_write.php?wp_hp_field=<?=$wp_hp_field?>" class="btn btn-outline-primary pull-right">Write</a>
 
         <!-- Pagination -->
-        <ul class="pagination justify-content-center" style="padding-bottom:40px; padding-top:30px;">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                    <span class="sr-only">Previous</span>
-                </a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">1</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">2</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">3</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </li>
-        </ul>
+<!--        <ul class="pagination justify-content-center" style="padding-bottom:40px; padding-top:30px;">-->
+<!--            <li class="page-item">-->
+<!--                <a class="page-link" href="#" aria-label="Previous">-->
+<!--                    <span aria-hidden="true">&laquo;</span>-->
+<!--                    <span class="sr-only">Previous</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--            <li class="page-item">-->
+<!--                <a class="page-link" href="#">1</a>-->
+<!--            </li>-->
+<!--            <li class="page-item">-->
+<!--                <a class="page-link" href="#">2</a>-->
+<!--            </li>-->
+<!--            <li class="page-item">-->
+<!--                <a class="page-link" href="#">3</a>-->
+<!--            </li>-->
+<!--            <li class="page-item">-->
+<!--                <a class="page-link" href="#" aria-label="Next">-->
+<!--                    <span aria-hidden="true">&raquo;</span>-->
+<!--                    <span class="sr-only">Next</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--        </ul>-->
     </div>
     <!-- /.container -->
     <? include("header.php"); ?>
