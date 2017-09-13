86a5df9f8327dba585718a1646aca7e9de967e61
hyunji_jung <hennyjung92@gmail.com>
2017-09-13 13:39:00 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -32,10 +32,10 @@
                     <a href="#" style="color: lightgray;"><img src="img/flag_ko.png" class="img-responsive"> Korean</a>
                 </li>
                 <li style="display: block; margin-bottom:7px;">
-                    <a href="#" style="color: #888;"><img src="img/flag_en.png" class="img-responsive"> English</a>
+                    <a href="#" style="color: lightgray;"><img src="img/flag_en.png" class="img-responsive"> English</a>
                 </li>
             </ul>
-
+            <ul style="width:200px;"></ul>
             <ul style="font-size: 14px; list-style: none; float:left; width:300px; color: #505050;">
                 <li class="m-0 text-right text-white" style="display: block; margin-bottom:7px;">82-70-7517-6246</li>
                 <li class="m-0 text-right text-white" style="display: block; margin-bottom:7px;">help@whopet.net</li>
