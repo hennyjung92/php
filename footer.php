1e5c8c71acb857df19446583eb498b71437add22
hyunji_jung <hennyjung92@gmail.com>
2017-09-13 11:27:59 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -16,35 +16,6 @@
 <footer class="py-5 bg-dark">
     <div class="container">
         <div class="row">
-<!--            <div class="col-lg-3 col-sm-3" style="padding-top:20px;">-->
-<!--                <a class="btn" href="https://www.facebook.com/" target="_blank" style="color:white">-->
-<!--                    <i class="fa fa-facebook" aria-hidden="true"></i>-->
-<!--                </a>-->
-<!--                <a class="btn" href="https://www.instagram.com/" target="_blank" style="color:white">-->
-<!--                    <i class="fa fa-instagram" aria-hidden="true"></i>-->
-<!--                </a>-->
-<!--                <a class="btn" href="https://twitter.com/" target="_blank" style="color:white">-->
-<!--                    <i class="fa fa-twitter" aria-hidden="true"></i>-->
-<!--                </a>-->
-<!--                <a class="btn" href="https://www.youtube.com/" target="_blank" style="color:white">-->
-<!--                    <i class="fa fa-youtube-play" aria-hidden="true"></i>-->
-<!--                </a>-->
-<!--<!--                <a href="#" style="padding-left:50px;" onclick="korean()"><img src="img/flag_ko.png" class="img-responsive"></a>-->
-<!--<!--                <a href="#" onclick="english()"><img src="img/flag_en.png" class="img-responsive"></a>-->
-<!--            </div>-->
-<!--            <div class="col-lg-3 col-sm-3">-->
-<!--                <a href="#" style="padding-left:50px;" onclick="korean()"><img src="img/flag_ko.png" class="img-responsive"></a>-->
-<!--                <br>-->
-<!--                <a href="#" style="padding-left:50px;" onclick="english()"><img src="img/flag_en.png" class="img-responsive"></a>-->
-<!---->
-<!--            </div>-->
-<!--            <div class="col-lg-2 col-sm-2">-->
-<!--            </div>-->
-<!--            <div class="col-lg-4 col-sm-4">-->
-<!--                <p class="m-0 text-right text-white">82-70-7517-6246</p>-->
-<!--                <p class="m-0 text-right text-white">help@whopet.net</p>-->
-<!--                <p class="m-0 text-right text-white">Hyojung BD 4Floor 15-1,Seoul,Korea</p>-->
-<!--            </div>-->
             <ul>
                 <a class="btn" href="https://www.facebook.com/" target="_blank" style="color:white">
                     <i class="fa fa-facebook" aria-hidden="true"></i>
@@ -61,7 +32,7 @@
 
             <ul style="width:180px;">
                 <li>
-                    <a href="#"><img src="img/flag_ko.png" class="img-responsive">Korean</a>
+                    <img src="img/flag_ko.png" class="img-responsive"><a href="#">Korean</a>
                 </li>
                 <li>
                     <a href="#"><img src="img/flag_en.png" class="img-responsive">English</a>
