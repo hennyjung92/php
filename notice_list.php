b1ee64bfbbe67709747de671e442f40ca99f9427
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 10:33:40 +0900

    file delete test

--- notice_list.php
+++ notice_list.php
@@ -21,6 +21,14 @@ $result = mysql_query($query, $connect);
 $temp = mysql_fetch_array($result);
 $total = $temp[0];
 ?>
+<style>
+    .col-md-5 p{
+        font-family: 'Noto Sans KR', sans-serif;
+        line-height:1.8em;
+        margin-top:10px;
+    }
+
+</style>
 <section id="notice">
     <section class="py-5">
         <div class="container" style="padding-top:30px; padding-bottom:30px;">
@@ -39,9 +47,7 @@ $total = $temp[0];
             ?>
             <div class="row">
                 <div class="col-md-7">
-
                     <img class="img-fluid rounded mb-3 mb-md-0" src='./data/<?=$data[file01]?>' alt="" style="display: block; margin-left: auto; margin-right: auto;">
-
                 </div>
                 <div class="col-md-5">
                     <h3 style="font-weight: 400;"><?=$data[wp_hp_review_title]?></a></h3>
@@ -59,63 +65,6 @@ $total = $temp[0];
             $cnt++;
         }
         ?>
-
-        <!-- Project Two -->
-<!--        <div class="row">-->
-<!--            <div class="col-md-7">-->
-<!--                <a href="#">-->
-<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog2.jpg" alt="">-->
-<!--                </a>-->
-<!--            </div>-->
-<!--            <div class="col-md-5">-->
-<!--                <h3 style="font-weight: 400;">Notice 2</h3>-->
-<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
-<!--                <a class="btn btn-primary" href="#">View More-->
-<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--        </div>-->
-        <!-- /.row -->
-
-<!--        <hr>-->
-
-        <!-- Project Three -->
-<!--        <div class="row">-->
-<!--            <div class="col-md-7">-->
-<!--                <a href="#">-->
-<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog3.jpg" alt="">-->
-<!--                </a>-->
-<!--            </div>-->
-<!--            <div class="col-md-5">-->
-<!--                <h3 style="font-weight: 400;">Event 1</h3>-->
-<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
-<!--                <a class="btn btn-primary" href="#">View More-->
-<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--        </div>-->
-        <!-- /.row -->
-
-<!--        <hr>-->
-
-        <!-- Project Four -->
-<!--        <div class="row">-->
-<!---->
-<!--            <div class="col-md-7">-->
-<!--                <a href="#">-->
-<!--                    <img class="img-fluid rounded mb-3 mb-md-0" src="img/dog5.jpg" alt="">-->
-<!--                </a>-->
-<!--            </div>-->
-<!--            <div class="col-md-5">-->
-<!--                <h3 style="font-weight: 400;">Event 2</h3>-->
-<!--                <p>lor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem  illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>-->
-<!--                <a class="btn btn-primary" href="#">View More-->
-<!--                    <span class="glyphicon glyphicon-chevron-right"></span>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--        </div>-->
-        <!-- /.row -->
-<!--        <hr>-->
         <div class="row">
             <div class="col-sm-12">
                 <div class="text-center">
@@ -125,32 +74,6 @@ $total = $temp[0];
             </div>
         </div>
         <br><br>
-<!--        <a href="notice_write.php?wp_hp_field=--><?//=$wp_hp_field?><!--" class="btn btn-outline-primary pull-right">Write</a>-->
-<!--        --><?// include("paging.php"); ?>
-        <!-- Pagination -->
-<!--        <ul class="pagination justify-content-center" style="padding-bottom:40px; padding-top:30px;">-->
-<!--            <li class="page-item">-->
-<!--                <a class="page-link" href="#" aria-label="Previous">-->
-<!--                    <span aria-hidden="true">&laquo;</span>-->
-<!--                    <span class="sr-only">Previous</span>-->
-<!--                </a>-->
-<!--            </li>-->
-<!--            <li class="page-item">-->
-<!--                <a class="page-link" href="#">1</a>-->
-<!--            </li>-->
-<!--            <li class="page-item">-->
-<!--                <a class="page-link" href="#">2</a>-->
-<!--            </li>-->
-<!--            <li class="page-item">-->
-<!--                <a class="page-link" href="#">3</a>-->
-<!--            </li>-->
-<!--            <li class="page-item">-->
-<!--                <a class="page-link" href="#" aria-label="Next">-->
-<!--                    <span aria-hidden="true">&raquo;</span>-->
-<!--                    <span class="sr-only">Next</span>-->
-<!--                </a>-->
-<!--            </li>-->
-<!--        </ul>-->
     </div>
     <!-- /.container -->
     <? include("footer.php"); ?>
