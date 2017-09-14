4c6363830e1b7d393086c242cb8812155921f7eb
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 09:30:04 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -38,6 +38,7 @@
                 </li>
             </ul>
             </div>
+            <div class="col-lg-3 col-sm-3"></div>
             <div class="col-lg-3 col-sm-3">
             <ul style="width:230px;"></ul>
             <ul style="font-size: 14px; list-style: none; float:left; width:300px; color: #505050;">
