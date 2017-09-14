ac2971d4984a996be59ed8956b693d2abcde88a2
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 17:17:50 +0900

    file delete test

--- review_update.php
+++ review_update.php
@@ -9,7 +9,7 @@
 <? include("header.php"); ?>
 <script type="text/javascript">
     function cancel(){
-        location.href="review_list.php";
+        location.href="review_list.php?wp_hp_field=<?=$wp_hp_field?>";
     }
 </script>
 <?
