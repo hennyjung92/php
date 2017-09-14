5455f57902862204c113247ba65990bde12f429c
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 17:20:09 +0900

    file delete test

--- noticeUpdateAction.php
+++ noticeUpdateAction.php
@@ -11,10 +11,12 @@ $wp_hp_review_summary=$_POST[wp_hp_review_summary];
 $wp_hp_review_content = $_POST[wp_hp_review_content];
 $wp_hp_review_no = $_POST[wp_hp_review_no];
 $wp_hp_field = $_POST[wp_hp_field];
+$file01 = $_POST[file01];
 
 if(!$wp_hp_review_title)Error("제목을 입력하세요.");
 if(!$wp_hp_review_summary)Error("요약을 입력하세요.");
 if(!$wp_hp_review_content)Error("내용을 입력하세요.");
+if(!$file01)Error("파일을 첨부해주세요.");
 
 if($_FILES[file01][name]){
     $_FILES['file01']['size'];
