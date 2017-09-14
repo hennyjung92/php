a46f84bff917348d061d4401ecccd08a2c306b21
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 09:46:33 +0900

    file delete test

--- join.php
+++ join.php
@@ -21,30 +21,7 @@
 </section>
 <div class="container">
     <div class="row">
-<!--        <div class="col-lg-4"></div>-->
-<!--        <div class="col-lg-4" style="padding-top:30px;">-->
-<!--            <div class="jumbotron" style="padding-top:30px;">-->
-<!--                <form method="post" action="joinAction.php">-->
-<!--                    <h4 style="text-align:center">회원가입</h4><br>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="text" class="form-control" placeholder="아이디를 입력하세요." name="wp_hp_id">-->
-<!--                    </div>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="password" class="form-control" placeholder="비밀번호를 입력하세요." name="wp_hp_password">-->
-<!--                    </div>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="text" class="form-control" placeholder="이름을 입력하세요." name="wp_hp_name">-->
-<!--                    </div>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="email" class="form-control" placeholder="이메일을 입력하세요." name="wp_hp_email">-->
-<!--                    </div>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="tel" class="form-control" placeholder="휴대폰 번호를 입력하세요." name="wp_hp_tel"><br>-->
-<!--                    </div>-->
-<!--                    <input type="submit" class="btn btn-primary form-control" value="가입완료">-->
-<!--                </form>-->
-<!--            </div>-->
-<!--        </div>-->
+
         <div class="col-lg-4 col-sm-4"></div>
         <div class="col-lg-4 col-sm-4">
             <hr>
