db13c999dbe623b5c96eb361a3a8310f49e11b1b
hyunji_jung <hennyjung92@gmail.com>
2017-09-14 09:29:02 +0900

    file delete test

--- footer.php
+++ footer.php
@@ -12,6 +12,7 @@
 <footer class="py-5 bg-dark">
     <div class="container">
         <div class="row">
+            <div class="col-lg-3 col-sm-3">
             <ul style="list-style: none; float:left; width:250px; color: #505050;">
                 <a class="btn" href="https://www.facebook.com/" target="_blank" style="color:white">
                     <i class="fa fa-facebook" aria-hidden="true"></i>
@@ -26,7 +27,8 @@
                     <i class="fa fa-youtube-play" aria-hidden="true"></i>
                 </a>
             </ul>
-
+            </div>
+            <div class="col-lg-3 col-sm-3">
             <ul style="width:180px; font-size: 12px; list-style: none; float:left; width:300px; color: #505050;">
                 <li style="display: block; margin-bottom:7px;">
                     <a href="#" style="color: lightgray;" onclick="korean()"><img src="img/flag_ko.png" class="img-responsive"> Korean</a>
@@ -35,12 +37,15 @@
                     <a href="#" style="color: lightgray;" onclick="english()"><img src="img/flag_en.png" class="img-responsive"> English</a>
                 </li>
             </ul>
+            </div>
+            <div class="col-lg-3 col-sm-3">
             <ul style="width:230px;"></ul>
             <ul style="font-size: 14px; list-style: none; float:left; width:300px; color: #505050;">
                 <li class="m-0 text-right text-white" style="display: block; margin-bottom:7px;">82-70-7517-6246</li>
                 <li class="m-0 text-right text-white" style="display: block; margin-bottom:7px;">help@whopet.net</li>
                 <li class="m-0 text-right text-white" style="display: block; margin-bottom:7px;">Hyojung BD 4Floor 15-1,Seoul,Korea</li>
             </ul>
+            </div>
         </div>
     </div>
 </footer>
