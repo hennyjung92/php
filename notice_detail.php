89a929d881b072a5fffd09e73082cbcd6430ce52
hyunji_jung <hennyjung92@gmail.com>
2017-09-15 11:13:42 +0900

    file delete test

--- notice_detail.php
+++ notice_detail.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>NOTICE</title>
+    <title>NOTICE DETAIL</title>
     <? include("head.php"); ?>
 
 </head>
