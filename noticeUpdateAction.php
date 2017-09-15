e52170402c32d35fb258894988bea06ee2e25d75
hyunji_jung <hennyjung92@gmail.com>
2017-09-15 10:44:11 +0900

    file delete test

--- noticeUpdateAction.php
+++ noticeUpdateAction.php
@@ -11,7 +11,7 @@ $wp_hp_review_summary=$_POST[wp_hp_review_summary];
 $wp_hp_review_content = $_POST[wp_hp_review_content];
 $wp_hp_review_no = $_POST[wp_hp_review_no];
 $wp_hp_field = $_POST[wp_hp_field];
-$oldfile01 = $_POST[oldfile01];
+
 if(!$wp_hp_review_title)Error("제목을 입력하세요.");
 if(!$wp_hp_review_summary)Error("요약을 입력하세요.");
 if(!$wp_hp_review_content)Error("내용을 입력하세요.");
@@ -40,7 +40,7 @@ if($_FILES[file01][name]){
               where wp_hp_review_no = '$wp_hp_review_no'";
     mysql_query($query,$connect);
 }
-if(!$file01 && !$newFile01)Error("파일을 첨부해주세요.");
+if(!$file01)Error("파일을 첨부해주세요.");
 
 $query = "update wp_hp_reviewBBS 
           set wp_hp_review_title='$wp_hp_review_title',
