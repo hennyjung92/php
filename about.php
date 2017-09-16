53a09978cacf21e92a6a952e442af305429797f8
hyunji_jung <hennyjung92@gmail.com>
2017-09-16 16:53:03 +0900

    file delete test

--- about.php
+++ about.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>ABOUT</title>
+<title>ABOUT</title>
 <? include("head.php"); ?>
-</head>
 <style>
     p{
         font-family: 'Noto Sans KR', sans-serif;
