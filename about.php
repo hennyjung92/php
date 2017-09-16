1f6f581a111ca35024dbdd11647f6c00bb53c8f7
hyunji_jung <hennyjung92@gmail.com>
2017-09-16 17:01:40 +0900

    file delete test

--- about.php
+++ about.php
@@ -20,8 +20,7 @@
 </style>
 <body>
 <? include("header.php"); ?>
-<section class="py-5 bg-image-full" style="background-image: url('img/boyanddog2.jpg');background-repeat: no-repeat;
-background-position: center left; background-attachment: fixed; opacity:0.8 !important;">
+<section class="py-5 bg-image-full" style="background-image: url('img/boyanddog2.jpg');opacity:0.8 !important;">
     <div style="height: 250px;"></div>
 </section>
 <section class="py-5">
