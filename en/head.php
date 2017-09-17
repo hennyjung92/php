fc13af54db34f217c2a7b09d214c7afc48fd45a1
hyunji_jung <hennyjung92@gmail.com>
2017-09-17 23:15:53 +0900

    Edit modern-css

--- en/head.php
+++ en/head.php
@@ -6,19 +6,19 @@
 <meta name="author" content="">
 
 <!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="icon" href="img/whopet_logo.png" type="image/png" sizes="16x16">
+<link rel="icon" href="../img/whopet_logo.png" type="image/png" sizes="16x16">
 <!-- Custom styles for this template -->
-<link href="css/modern-business.css" rel="stylesheet">
-<link href="css/full-width-pics.css" rel="stylesheet">
+<link href="../css/modern-business.css" rel="stylesheet">
+<link href="../css/full-width-pics.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/popper/popper.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="../vendor/popper/popper.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
 </head>
