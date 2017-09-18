a907396792738f450520b3f151a05e108bf27ea1
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 17:09:06 +0900

    edit services

--- en/about.php
+++ en/about.php
@@ -26,8 +26,7 @@
 <section class="py-5">
     <div class="container">
         <h1 class="text-center">Why Choose Us</h1>
-
-        <p class="text-center" style="font-weight: 500;">"기존 반려동물 위치추적기와 WHOPET의 차이점"</p>
+        <p class="text-center" style="font-weight: 500;">"Difference between existing companion animal locator and WHOPET"</p>
     </div>
 </section>
 <section>
@@ -41,8 +40,12 @@
             <div class="col-md-6 order-1">
                 <div class="p-5">
                     <h2 class="display-4">Building</h2>
-                    <p style="font-weight: 700;">지하/건물 내부에서 추적 가능</p>
-                    <p>기존에는 지하나 건물 내부에서 위치 추적이 불가능했지만<br>WHOPET은 지하나 건물 내부에서도<br>반려동물의 정확한 위치 추적이 가능합나다.</p>
+                    <p style="font-weight: 700;">Can be tracked in the basement / inside the building</p>
+                    <p>
+                    Previously, it was not possible to track the location in the basement and inside the building,
+                    but WHOPET can track the exact location of the companion animal in the underground and inside the building.
+                    </p>
+<!--                    <p>기존에는 지하나 건물 내부에서 위치 추적이 불가능했지만<br>WHOPET은 지하나 건물 내부에서도<br>반려동물의 정확한 위치 추적이 가능합나다.</p>-->
                 </div>
             </div>
         </div>
@@ -60,8 +63,13 @@
             <div class="col-md-6">
                 <div class="p-5">
                     <h2 class="display-4">Map</h2>
-                    <p style="font-weight: 700;">연결과 동시에 지도로 추적</p>
-                    <p>일정거리 이상 벗어날 경우, 단순 분실 알림이 아니라<br>map 상에서 알림과 동시에 추적 기능을 제공합니다.</p>
+                    <p style="font-weight: 700;">Track on the map simultaneously with the connection</p>
+                    <p>
+                        If it goes beyond a certain distance,
+                        it provides notifications on the map as well as a tracking function
+                        instead of a simple loss notification.
+                    </p>
+<!--                    <p>일정거리 이상 벗어날 경우, 단순 분실 알림이 아니라<br>map 상에서 알림과 동시에 추적 기능을 제공합니다.</p>-->
                 </div>
             </div>
         </div>
@@ -79,8 +87,12 @@
             <div class="col-md-6 order-1">
                 <div class="p-5">
                     <h2 class="display-4">PC Available</h2>
-                    <p style="font-weight: 700;">스마트폰 없어도 PC로 추적 가능</p>
-                    <p>기존에는 스마트폰이 없으면 위치 추적 서비스가 불가했지만<br>WHOPET은 스마트폰이 없이도 PC에서 위치 추적이 가능합니다.</p>
+                    <p style="font-weight: 700;">Can trace to PC without smartphone</p>
+<!--                    <p>기존에는 스마트폰이 없으면 위치 추적 서비스가 불가했지만<br>WHOPET은 스마트폰이 없이도 PC에서 위치 추적이 가능합니다.</p>-->
+                    <p>
+                        In the past, location tracking service was not available without a smartphone,
+                        but WHOPET can track the location on a PC without a smartphone.
+                    </p>
                 </div>
             </div>
         </div>
@@ -98,8 +110,9 @@
             <div class="col-md-6">
                 <div class="p-5">
                     <h2 class="display-4">Unlimited Service</h2>
-                    <p style="font-weight: 700;">디바이스 구매시 무한 서비스 제공</p>
-                    <p>통신 서비스 가입 후 매달 사용료를 지불하는 방식이 아닌!<br>디바이스 구매 시 무한 서비스를 제공합니다.</p>
+                    <p style="font-weight: 700;">Provides unlimited service for device purchase</p>
+<!--                    <p>통신 서비스 가입 후 매달 사용료를 지불하는 방식이 아닌!<br>디바이스 구매 시 무한 서비스를 제공합니다.</p>-->
+                    <p>We provide unlimited service when purchasing device, not monthly payment after monthly subscription.</p>
                 </div>
             </div>
         </div>
