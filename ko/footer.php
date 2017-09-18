cb3629daf00a08c74ff3bd537880574a28c82931
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 10:39:02 +0900

    edit services

--- ko/footer.php
+++ ko/footer.php
@@ -27,8 +27,8 @@
                 </li>
             </ul>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"></div>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-3"></div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-3">
             <ul style="font-size: 14px; list-style: none; float:left; width:300px; color: #505050;">
                 <li class="m-0 text-white" style="display: block; margin-bottom:7px;">82-70-7517-6246</li>
                 <li class="m-0 text-white" style="display: block; margin-bottom:7px;">help@whopet.net</li>
