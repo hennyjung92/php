1d5977a4352dd4db42545c4f090e3b4d55ade0bb
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 11:45:48 +0900

    edit services

--- ko/qna.php
+++ ko/qna.php
@@ -21,11 +21,7 @@
 </section>
 <div class="container">
     <div class="row" style="padding-top:20px; padding-bottom:60px;">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-            <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">
-                <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>
-            </a>
-            <p class="ns" style="font-size:15px; padding-top: 30px;">디바이스</p>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
         </div>
         <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
             <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">
@@ -38,6 +34,14 @@
                 <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>
             </a>
             <p class="ns" style="font-size:15px; padding-top: 30px;">디바이스</p>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+            <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">
+                <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>
+            </a>
+            <p class="ns" style="font-size:15px; padding-top: 30px;">디바이스</p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
         </div>
     </div>
 </div>
