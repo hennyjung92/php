d09e87b23164eac9baab31f7abd248a06ebcf44b
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 15:13:51 +0900

    edit services

--- ko/about.php
+++ ko/about.php
@@ -29,29 +29,35 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <!-- Slide One - Set the background image for this slide in the line below -->
-            <div class="carousel-item active" style="background-image: url(../../img/main.jpg); height: 250px; opacity:0.8 !important;“>
+            <div class="carousel-item active" style="background-image: url(../../img/main.jpg);height: 250px; opacity:0.8 !important;">
                 <div class="carousel-caption d-none d-md-block">
+                    <!-- <h3>First Slide</h3>
+                    <p>This is a description for the first slide.</p> -->
+                </div>
+            </div>
+            <!-- Slide Two - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url(../../img/dog1.jpg);height: 250px; opacity:0.8 !important;">
+                <div class="carousel-caption d-none d-md-block">
+                    <!-- <h3>Second Slide</h3>
+                    <p>This is a description for the second slide.</p> -->
+                </div>
+            </div>
+            <!-- Slide Three - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url(../../img/dog4.jpg);height: 250px; opacity:0.8 !important;">
+                <div class="carousel-caption d-none d-md-block">
+                    <!-- <h3>Third Slide</h3>
+                    <p>This is a description for the third slide.</p> -->
+                </div>
+            </div>
         </div>
-    </div>
-    <!-- Slide Two - Set the background image for this slide in the line below -->
-    <div class="carousel-item" style="background-image: url(../../img/dog1.jpg); height: 250px; opacity:0.8 !important;“>
-                <div class="carousel-caption d-none d-md-block">
-    </div>
-    </div>
-    <!-- Slide Three - Set the background image for this slide in the line below -->
-    <div class="carousel-item" style="background-image: url(../../img/dog4.jpg); height: 250px; opacity:0.8 !important;“>
-                <div class="carousel-caption d-none d-md-block">
-    </div>
-    </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </header>
 
