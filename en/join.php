814dcc3cabab4f2781400dc2a8b387ef4e44317d
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 10:33:11 +0900

    edit services

--- en/join.php
+++ en/join.php
@@ -7,13 +7,12 @@
 <section class="py-5">
     <div class="container" style="padding-top:30px; padding-bottom:30px;">
         <h1 class="jg text-center">회원가입</h1>
-    </div>
+            </div>
 </section>
 <div class="container">
     <div class="row">
-
-        <div class="col-lg-4 col-sm-4"></div>
-        <div class="col-lg-4 col-sm-4">
+        <div class="col-lg-4 col-md-3 col-sm-3 col-xs-3"></div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
             <hr>
         <form method="post" action="joinAction.php">
             <div class="form-group">
@@ -46,7 +45,7 @@
             </div>
         </form>
         </div>
-        <div class="col-lg-4 col-sm-4"></div>
+        <div class="col-lg-4 col-md-3 col-sm-3 col-xs-3"></div>
     </div>
 </div>
 <br><br><br>
