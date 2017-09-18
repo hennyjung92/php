0169c3738f4638308690d109029595d96564d1ee
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 17:17:02 +0900

    edit services

--- en/service.php
+++ en/service.php
@@ -26,19 +26,23 @@
         <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 text-center">
                 <i class="fa fa-map-o fa-2x" id="service_map" aria-hidden="true"></i>
-                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 실내외, 지상, 지하<br>범위 제한 없이 위치 추적</p>
+<!--                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 실내외, 지상, 지하<br>범위 제한 없이 위치 추적</p>-->
+                <p class="ns" style="font-size:15px; padding-top: 30px;">Track your location in real time <br>indoors, outdoors, ground, underground</p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 text-center">
                 <i class="fa fa-bar-chart fa-2x" id="service_chart" aria-hidden="true"></i>
-                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 분실 방지 및 통계</p>
+<!--                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 분실 방지 및 통계</p>-->
+                <p class="ns" style="font-size:15px; padding-top: 30px;">Real-time loss prevention and statistics</p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 text-center">
                 <i class="fa fa-sign-out fa-2x" id="service_signout" aria-hidden="true"></i>
-                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 나의 위치 및<br>재난 대피 경로 파악</p>
+<!--                <p class="ns" style="font-size:15px; padding-top: 30px;">실시간 나의 위치 및<br>재난 대피 경로 파악</p>-->
+                <p class="ns" style="font-size:15px; padding-top: 30px;">Identify my real-time location and disaster evacuation routes</p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 text-center">
                 <i class="fa fa-cog fa-2x" id="service_cog" aria-hidden="true"></i>
-                <p class="ns" style="font-size:15px; padding-top: 30px;">Mobile App, PC Web을 통한<br>내 애완동물 위치 파악 및 상태 관리 기능</p>
+<!--                <p class="ns" style="font-size:15px; padding-top: 30px;">Mobile App, PC Web을 통한<br>내 애완동물 위치 파악 및 상태 관리 기능</p>-->
+                <p class="ns" style="font-size:15px; padding-top: 30px;">My pet location and Status management function<br> via mobile apps and PC web</p>
             </div>
         </div>
     </div>
