86dcfac1b6289e443bf1f883ffe8134f79d0b8df
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 11:42:50 +0900

    edit services

--- ko/qna.php
+++ ko/qna.php
@@ -20,7 +20,7 @@
 
 </section>
 <div class="container">
-    <div class="row">
+    <div class="row" style="padding-top:20px; padding-bottom:60px;">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center">
             <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">
                 <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>
@@ -46,9 +46,9 @@
 <!--        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">-->
 <!--        </div>-->
 <!--        <div class="h4 col-lg-2 col-md-2 col-sm-2 col-xs-2 text-center mb-4">-->
-            <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">
-                <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>
-            </a>
+<!--            <a class="btn" href="review_list.php?wp_hp_field=02" style="color:black">-->
+<!--                <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i>-->
+<!--            </a>-->
 <!--            <div class="card-body">-->
 <!--                <p class="card-text">디바이스</p>-->
 <!--            </div>-->
