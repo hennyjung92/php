2490f42a8e83566f4fec10cc0ca96ae8511bc244
hyunji_jung <hennyjung92@gmail.com>
2017-09-18 10:07:57 +0900

    edit services

--- ko/join.php
+++ ko/join.php
@@ -12,8 +12,8 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-lg-4 col-sm-4"></div>
-        <div class="col-lg-4 col-sm-4">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12"></div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
             <hr>
         <form method="post" action="joinAction.php">
             <div class="form-group">
@@ -46,7 +46,7 @@
             </div>
         </form>
         </div>
-        <div class="col-lg-4 col-sm-4"></div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12"></div>
     </div>
 </div>
 <br><br><br>
