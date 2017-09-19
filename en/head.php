eeee91e6de7c744b8e2af6a5b8e25b5d2f80c6c9
hyunji_jung <hennyjung92@gmail.com>
2017-09-19 12:39:20 +0900

    ie meta tag 추가

--- en/head.php
+++ en/head.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Bootstrap core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
