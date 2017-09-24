0ce7877e9ed7c28352a01841234ef47e4ab25c0e
hyunji_jung <hennyjung92@gmail.com>
2017-09-24 19:26:57 +0900

    search bbs test

--- ko/review_list.php
+++ ko/review_list.php
@@ -82,7 +82,7 @@ $view_total = 10; // 한 페이지에 보이는 수
 if(!$_page)($_page=1); // 페이지 번호가 지정이 안되었을 경우
 $page = ($_page-1)*$view_total;
 
-//$where ="wp_hp_review_no";
+$where ="wp_hp_review_no";
 if($search_text){
     if($search_mode==1) $tmp="wp_hp_review_title";
     if($search_mode==2) $tmp="wp_hp_review_content";
