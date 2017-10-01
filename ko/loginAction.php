fd1f4ff9de4bbc3ade0364423731488a41fd4637
hyunji_jung <hennyjung92@gmail.com>
2017-10-02 02:01:00 +0900

    search bbs test

--- ko/loginAction.php
+++ ko/loginAction.php
@@ -13,19 +13,37 @@ mysql_query("set names utf8",$connect);
 $result = mysql_query($query,$connect);
 $wp_hp_member = mysql_fetch_array($result);
 
-if(!$wp_hp_id)Error("아이디를 입력하세요.");
-elseif(!$wp_hp_member[wp_hp_id])Error("존재하지 않는 회원 아이디입니다.");
+//if(!$wp_hp_id)Error("아이디를 입력하세요.");
+//elseif(!$wp_hp_member[wp_hp_id])Error("존재하지 않는 회원 아이디입니다.");
 
-if(!$wp_hp_password)Error("비밀번호를 입력하세요");
-elseif($wp_hp_member[wp_hp_password]!=$wp_hp_password)Error("비밀번호가 잘못되었습니다.");
+if(!$wp_hp_id){
+    echo"<script>
+    window.alert('아이디 입력하세요');
+</script>";
+    return false;
+}
+if(!$wp_hp_password){
+    echo"<script>
+    window.alert('비밀번호를 입력하세요');
+</script>";
+    return false;
+}
+elseif($wp_hp_member[wp_hp_password]!=$wp_hp_password){
+    echo"<script>
+    window.alert('비밀번호가 잘못되었습니다.');
+</script>";
+    return false;
+}
 
 if($wp_hp_member[wp_hp_id] and $wp_hp_member[wp_hp_password]==$wp_hp_password) {
 session_start();
 $_SESSION['wp_hp_id']=$wp_hp_member[wp_hp_id];
+echo"
+<script>
+window.alert(\"<?=$wp_hp_member[wp_hp_id];?>님 환영합니다!\");
+location.href=\"index.php\";
+</script>
+";
 }
 ?>
 
-<script>
-window.alert("<?=$wp_hp_member[wp_hp_id];?>님 환영합니다!");
-location.href="index.php";
-</script>
