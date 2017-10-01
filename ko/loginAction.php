e0e6ef0117c35809d0609016f1eceed5bc680953
hyunji_jung <hennyjung92@gmail.com>
2017-10-02 01:33:11 +0900

    search bbs test

--- ko/loginAction.php
+++ ko/loginAction.php
@@ -13,8 +13,14 @@ mysql_query("set names utf8",$connect);
 $result = mysql_query($query,$connect);
 $wp_hp_member = mysql_fetch_array($result);
 
-if(!$wp_hp_id){
-    Error("아이디를 입력하세요");}
+if(!$wp_hp_id) {
+    echo "
+    <script>
+    window.alert('$msg');
+    history.back();
+</script>
+";
+}
 elseif(!$wp_hp_member[wp_hp_id])Error("존재하지 않는 회원 아이디입니다.");
 
 if(!$wp_hp_password){
