8de49b8b467cc54c732a61b0a3eff03fb534190f
hyunji_jung <hennyjung92@gmail.com>
2017-10-03 00:24:56 +0900

    join ajax test

--- ko/join.php
+++ ko/join.php
@@ -17,72 +17,73 @@
             <form method="post" id="join_form">
             <div class="form-group">
                 <label>아이디</label>
-                <input type="text" class="form-control" placeholder="아이디를 입력하세요.(4-12자리 영문 및 숫자)" name="wp_hp_id" id="wp_hp_id" minlength="4" maxlength="12" autofocus="">
+                <input type="text" class="form-control" placeholder="아이디를 입력하세요.(4-12자리 영문 및 숫자)" name="wp_hp_id" minlength="4" maxlength="12" autofocus="">
             </div>
             <div class="form-group">
                 <label>비밀번호</label>
-                <input type="password" class="form-control" placeholder="비밀번호를 입력하세요." name="wp_hp_password" id="wp_hp_password">
+                <input type="password" class="form-control" placeholder="비밀번호를 입력하세요." name="wp_hp_password">
             </div>
             <div class="form-group">
                 <label>비밀번호 확인</label>
-                <input type="password" class="form-control" placeholder="비밀번호 다시 한번 더 입력하세요." name="wp_hp_password_check" id="wp_hp_password_check">
+                <input type="password" class="form-control" placeholder="비밀번호 다시 한번 더 입력하세요." name="wp_hp_password_check">
             </div>
             <div class="form-group">
                 <label>이름</label>
-                <input type="text" class="form-control" placeholder="이름을 입력하세요." name="wp_hp_name" id="wp_hp_name">
+                <input type="text" class="form-control" placeholder="이름을 입력하세요." name="wp_hp_name">
             </div>
             <div class="form-group">
                 <label>이메일</label>
-                <input type="email" class="form-control" placeholder="이메일을 입력하세요." name="wp_hp_email" id="wp_hp_email">
+                <input type="email" class="form-control" placeholder="이메일을 입력하세요." name="wp_hp_email">
             </div>
             <div class="form-group">
                 <label>휴대폰 번호</label>
-                <input type="tel" class="form-control" placeholder="휴대폰 번호를 입력하세요." name="wp_hp_tel" id="wp_hp_tel"><br>
+                <input type="tel" class="form-control" placeholder="휴대폰 번호를 입력하세요." name="wp_hp_tel"><br>
             </div>
             <hr>
             <div class="text-center">
 <!--            <input type="submit" class="btn btn-outline-primary" value="가입하기">-->
-                <button type="button" id="join_button" name="join_button" class="btn btn-primary center-block">Register</button>
+                <button type="button" id="join_button" name="join_button" class="btn btn-primary center-block">가입하기</button>
             </div>
         </form>
         </div>
         <div class="col-lg-4 col-md-2 col-sm-2 col-xs-3"></div>
     </div>
 </div>
+<br><br><br>
 
 <script>
     $(document).ready(function(){
         $('#join_button').click(function(event){
-            var wp_hp_id =$('#wp_hp_id').val();
-            var wp_hp_password =$('#wp_hp_password').val();
-            var wp_hp_password_check =$('#wp_hp_password_check').val();
-            var wp_hp_name =$('#wp_hp_name').val();
-            var wp_hp_email =$('#wp_hp_email').val();
-            var wp_hp_tel =$('#wp_hp_tel').val();
-
+            var wp_hp_id =$('input[name=wp_hp_id]').val();
+            var wp_hp_password =$('input[name=wp_hp_password]').val();
+            var wp_hp_password_check =$('input[name=wp_hp_password_check]').val();
+            var wp_hp_name =$('input[name=wp_hp_name]').val();
+            var wp_hp_email =$('input[name=wp_hp_email]').val();
+            var wp_hp_tel =$('input[name=wp_hp_tel]').val();
+            alert(wp_hp_id);
             if(wp_hp_id==''){
                 alert("ID is required.");
-                $('#wp_hp_id').focus();
+                $('input[name=wp_hp_id]').focus();
             }
             else if(wp_hp_password==''){
                 alert("password is required.");
-                $('#wp_hp_password').focus();
+                $('input[name=wp_hp_password]').focus();
             }
             else if(wp_hp_password_check==''){
                 alert("please input your password again.");
-                $('#wp_hp_password_check').focus();
+                $('input[name=wp_hp_password_check]').focus();
             }
             else if(wp_hp_name==''){
                 alert("name is required.");
-                $('#wp_hp_name').focus();
+                $('input[name=wp_hp_name]').focus();
             }
             else if(wp_hp_email==''){
                 alert("email address is required.");
-                $('#wp_hp_email').focus();
+                $('input[name=wp_hp_email]').focus();
             }
             else if(wp_hp_tel==''){
                 alert("phone number is required.");
-                $('#wp_hp_tel').focus();
+                $('input[name=wp_hp_tel]').focus();
             }
             else{
                 $.ajax({
@@ -92,15 +93,15 @@
                     success:function(data){
                         if(data==0){
                             alert("존재하는 아이디입니다. 다른 아이디를 입력해주세요.");
-                            $('#wp_hp_id').focus();
+                            $('input[name=wp_hp_id]').focus();
                         }
                         else if(data==1){
                             alert("아이디는 영문 소/대문자와 숫자만 허용됩니다.");
-                            $('#wp_hp_id').focus();
+                            $('input[name=wp_hp_id]').focus();
                         }
                         else if (data==2){
                             alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
-                            $('#wp_hp_password').focus();
+                            $('input[name=wp_hp_password]').focus();
                         }
                         else{
                             alert("회원가입이 완료되었습니다.");
@@ -112,8 +113,6 @@
         });
     });
 </script>
-
-<br><br><br>
 
 <? include("footer.php"); ?>
 </body>
