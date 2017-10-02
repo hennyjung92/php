8171a8c5a86cdb7485f429c31209847633a765ff
hyunji_jung <hennyjung92@gmail.com>
2017-10-02 18:39:44 +0900

    search bbs test

--- ko/loginActionAjax.php
+++ ko/loginActionAjax.php
@@ -7,47 +7,19 @@ $wp_hp_id=$_POST[wp_hp_id];
 $wp_hp_password_security=$_POST[wp_hp_password];
 $wp_hp_password = md5($wp_hp_password_security); // 비번 암호화
 
-//if(isset($wp_hp_id)){
-//    $query = "select * from wp_hp_member where wp_hp_id='$wp_hp_id' and wp_hp_password='$wp_hp_password'";
-//    $result = mysql_query($query,$connect);
+if(isset($wp_hp_id)){
+    $query = "select * from wp_hp_member where wp_hp_id='$wp_hp_id' and wp_hp_password='$wp_hp_password'";
+    mysql_query("set names utf8",$connect);
+    $result = mysql_query($query,$connect);
 //    $wp_hp_member = mysql_fetch_array($result);
-//
-//    if(mysql_num_rows($result)>0)
-//    {
-//        $_SESSION['wp_hp_id'] = $wp_hp_member[wp_hp_id];
-//        echo 'yes';
-//    }
-//    else{
-//        echo 'no';
-//    }
-//}
 
-// 쿼리전송
-$query = "select * from wp_hp_member where wp_hp_id='$wp_hp_id'";
-mysql_query("set names utf8",$connect);
-$result = mysql_query($query,$connect);
-$wp_hp_member = mysql_fetch_array($result);
-
-//if(mysql_num_rows($result)>0)
-//{
-//    $_SESSION['wp_hp_id'] = $wp_hp_member[wp_hp_id];
-//    echo 'yes';
-//}
-//else{
-//    echo 'no';
-//}
-
-
-if($wp_hp_member[wp_hp_id] and $wp_hp_member[wp_hp_password]==$wp_hp_password) {
-    //session_start();
-    $_SESSION['wp_hp_id']=$wp_hp_member[wp_hp_id];
-    echo 'yes';
-}
-else{
-    echo 'no';
+    if(mysql_num_rows($result)>0) {
+        $_SESSION['wp_hp_id']=$wp_hp_id;
+        echo 'yes';
+    }
+    else{
+        echo 'no';
+    }
 }
 ?>
-<script>
-    window.alert("<?=$wp_hp_member[wp_hp_id];?>님 환영합니다!");
-    location.href="index.php";
-</script>
+
