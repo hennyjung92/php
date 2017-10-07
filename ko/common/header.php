9718f05350c6a6920f4651a1c7d0b7efcf6922f1
hyunji_jung <hennyjung92@gmail.com>
2017-10-08 01:49:34 +0900

    search bbs test

--- ko/common/header.php
+++ ko/common/header.php
@@ -7,11 +7,11 @@ $wp_hp_member = member();
 <script type="text/javascript">
     function management(){
         alert("Go to the Integrated Management System page.");
-        location.href="../../index.php";
+        location.href="../index.php";
     }
     function mypage(){
         alert("Go to MyPage");
-        location.href="../../index.php";
+        location.href="../index.php";
     }
 </script>
 
@@ -51,10 +51,10 @@ $wp_hp_member = member();
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="../about.php">ABOUT</a>
+                    <a class="nav-link" href="../ko/about.php">ABOUT</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../service.php">SERVICES</a>
+                    <a class="nav-link" href="../ko/service.php">SERVICES</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -62,9 +62,9 @@ $wp_hp_member = member();
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
 
-                        <a class="dropdown-item" href="../notice_list.php?wp_hp_field=05">NOTICE</a>
-                        <a class="dropdown-item" href="../review_list.php?wp_hp_field=01">REVIEW</a>
-                        <a class="dropdown-item" href="../gallery.php">GALLERY</a>
+                        <a class="dropdown-item" href="../ko/notice_list.php?wp_hp_field=05">NOTICE</a>
+                        <a class="dropdown-item" href="../ko/review_list.php?wp_hp_field=01">REVIEW</a>
+                        <a class="dropdown-item" href="../ko/gallery.php">GALLERY</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
@@ -73,8 +73,8 @@ $wp_hp_member = member();
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                        <a class="dropdown-item" href="../qna.php">Q&A</a>
-                        <a class="dropdown-item" href="../contact.php">CONTACT</a>
+                        <a class="dropdown-item" href="../ko/qna.php">Q&A</a>
+                        <a class="dropdown-item" href="../ko/contact.php">CONTACT</a>
                     </div>
                 </li>
                 <!--                <li class="nav-item">-->
@@ -210,7 +210,7 @@ $wp_hp_member = member();
                     }
                     else{
                         alert("회원가입이 완료되었습니다.");
-                        location.href="../../index.php";
+                        location.href="../index.php";
                     }
                 }
             }); // ajax
@@ -259,7 +259,7 @@ $wp_hp_member = member();
                         }
                         else{
                             alert("whopet에 오신 것을 환영합니다!");
-                            location.href="../../index.php";
+                            location.href="../index.php";
                         }
                     }
                 });
