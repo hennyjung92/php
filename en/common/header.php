6de81cb2b2de4751094aad32564d3742ffa48290
hyunji_jung <hennyjung92@gmail.com>
2017-10-18 09:56:10 +0900

    autofocus

--- en/common/header.php
+++ en/common/header.php
@@ -44,7 +44,7 @@ $wp_hp_member = member();
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-faded fixed-top" style="background-color:white;" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="../en/index.php"><img src="../../img/whopet_logo.png" class="img-responsive" height="50"></a>
+        <a class="navbar-brand" href="../index.php"><img src="../../img/whopet_logo.png" class="img-responsive" height="50"></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -77,7 +77,6 @@ $wp_hp_member = member();
                         <a class="dropdown-item" href="../en/contact.php">CONTACT</a>
                     </div>
                 </li>
-
                 <?
                 session_start();
                 $wp_hp_member[wp_hp_id] = $_SESSION['wp_hp_id'];
@@ -89,7 +88,7 @@ $wp_hp_member = member();
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
                             <a class="dropdown-item" href="#" onclick="management()"><i class="fa fa-cog" aria-hidden="true"> management</i></a>
-                            <a class="dropdown-item" href="../en/logout.php" onclick="return confirm('Are you sure you want to sign out?')"><i class="fa fa-sign-out" aria-hidden="true"> logout</i></a>
+                            <a class="dropdown-item" href="../en/logout.php" onclick="return confirm('로그아웃 하시겠습니까?')"><i class="fa fa-sign-out" aria-hidden="true"> logout</i></a>
                         </div>
                     </li>
                 <? }
@@ -99,13 +98,13 @@ $wp_hp_member = member();
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#myModal" name="login" id="login" data-toggle="modal"><i class="fa fa-circle-o-notch" aria-hidden="true"></i></a>
+                        <!--                        <a><button type="button" name="login" id="login" class="btn btn-success" data-toggle="modal" data-target="#myModal"><i class="fa fa-circle-o-notch" aria-hidden="true"></i></button> </a>-->
                     </li>
                 <? } ?>
             </ul>
         </div>
     </div>
 </nav>
-
 <!-- joinModal -->
 <div class="modal fade" id="joinModal" role="dialog">
     <div class="modal-dialog" style="width:350px;">
@@ -236,8 +235,8 @@ $wp_hp_member = member();
             </div>
             <div class="modal-body">
                 <form method="post">
-                    <input type="text" class="form-control" placeholder="Enter your ID." name="wp_hp_id" id="wp_hp_id" maxlength="20" autofocus=""><br>
-                    <input type="password" class="form-control" placeholder="Enter your password." name="wp_hp_password" id="wp_hp_password" maxlength="20"><br><br>
+                    <input type="text" class="form-control" placeholder="아이디를 입력해주세요." name="wp_hp_id" id="wp_hp_id" maxlength="20" autofocus=""><br>
+                    <input type="password" class="form-control" placeholder="비밀번호를 입력해주세요." name="wp_hp_password" id="wp_hp_password" maxlength="20"><br><br>
                     <span style="text-align:center;"><button type="button" id="login_button" name="login_button" class="btn btn-primary center-block">Login</button></span>
                 </form>
             </div>
