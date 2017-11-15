22e9fda21fbb580509ede845903d2cfed08fb4ad
hyunji_jung <hennyjung92@gmail.com>
2017-11-15 12:50:45 +0900

    contact 경로 수정

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-</head>
 <?
-echo "공사중";
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+if ($lang == "ko") { //index.php 설정이 한국어 일 때
+    echo("<script>location.href='./ko/index.php'
+            </script>");
+}
+
+elseif ($lang == "en"){ // 영어일 때
+    echo("<script>location.href='./en/index.php'
+            </script>");
+}
+
+else  { // 한국어 영어도 아닐 때
+    echo("<script>location.href='./en/index.php'
+            </script>");
+}
 ?>
